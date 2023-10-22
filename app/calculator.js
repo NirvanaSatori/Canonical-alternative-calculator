2f19c004245213a1d6a01e4e7d78a4a56edaa8bd
Your Name <Jmlsoteyza.dev@gmail.com>
2023-10-22 16:36:18 +0400

    canonical-test-assesment

---
+++ app/calculator.js
@@ -0,0 +1,44 @@
+exports.calculate = function (expression) {
+  let num = 0;
+  let tokens = expression.split(" ").reverse();
+
+  while (num < tokens.length) {
+    let operator = tokens[num];
+
+    if (
+      operator === "+" ||
+      operator === "-" ||
+      operator === "*" ||
+      operator === "/"
+    ) {
+      let currentIndex = tokens.findIndex((element) => element === operator);
+
+      if (operator === "-") {
+        tokens.splice(
+          currentIndex - 2,
+          3,
+          eval(
+            `${tokens[currentIndex - 1]} ${operator} ${
+              tokens[currentIndex - 2]
+            }`
+          )
+        );
+      } else {
+        tokens.splice(
+          currentIndex - 2,
+          3,
+          eval(
+            `${tokens[currentIndex - 2]} ${operator} ${
+              tokens[currentIndex - 1]
+            }`
+          )
+        );
+      }
+
+      num = currentIndex - 2;
+    }
+    num++;
+  }
+
+  return tokens[0];
+};
